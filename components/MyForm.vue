70e482a05c6a62924837f2848969beb42fa5b6a4
Ivana <kostevski.ivana@gmail.com>
2023-10-22 19:38:10 +0200

    fix: fix stylelint

---
+++ components/MyForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="contact-form-container">
+      <form id="form" class="contact-form">
+        <div class="input-container">
+          <div class="input-unit">
+            <input
+              id="name"
+              placeholder="service-name"
+              type="text"
+              class="input-contact-info"
+              autocomplete="name"
+              required
+            />
+            <p></p>
+          </div>
+          <div class="input-unit">
+            <input
+              id="email"
+              placeholder="business-name"
+              type="email"
+              class="input-contact-info"
+              autocomplete="email"
+              required
+            />
+            <p></p>
+          </div>
+          <div class="input-unit">
+            <input
+              id="rating"
+              placeholder="service-rating"
+              type="email"
+              class="input-contact-info"
+              autocomplete="email"
+              required
+            />
+            <p></p>
+          </div>
+        </div>
+
+        <input type="submit" value="Submit" class="submit-btn" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<style>
+div {
+  color: red;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,22 @@
   <div>
     <NuxtLink to="/experience">Experience</NuxtLink>
     <p>Hello!</p>
+    <MyForm />
   </div>
 </template>
 
 <script>
+import MyForm from '~/components/MyForm.vue'
+
 export default {
   name: 'IndexPage',
+  components: {
+    MyForm,
+  },
 }
 </script>
+<style>
+div {
+  background-color: red;
+}
+</style>
