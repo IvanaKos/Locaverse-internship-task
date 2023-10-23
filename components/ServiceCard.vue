36eda8b8d8fada9fdec9fbe9710405b898355aed
Ivana <kostevski.ivana@gmail.com>
2023-10-23 11:50:17 +0200

    chore: add ServiceCard component

---
+++ components/ServiceCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="service-card-wraper">
+    <div class="service-card">
+      <div class="service-img">
+        <img src="../assets/img/locaverse-logo.jpg" alt="" />
+      </div>
+      <div class="service-info">
+        <p class="service-name">This is service name</p>
+        <p class="business-name">This is business name</p>
+        <p class="rating">Rate me</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.service-card-wraper {
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+  row-gap: 0.5rem;
+  margin: 80px;
+}
+
+.service-card {
+  width: 210px;
+  height: 330px;
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+.service-img {
+  width: 100%;
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.service-img img {
+  max-width: 100%;
+}
+
+.service-info {
+  width: 100%;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  padding: 10px;
+  border: 1px solid hsl(206deg 14% 68%);
+  border-radius: 0 0 5px 5px;
+  overflow: hidden;
+}
+
+.service-name,
+.business-name {
+  line-height: 1;
+}
+
+.service-name {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+.business-name {
+  font-size: 1.1rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,21 @@
   <div class="page">
     <MainHeader />
     <MyForm />
+    <ServiceCard />
   </div>
 </template>
 
 <script>
 import MyForm from '~/components/MyForm.vue'
 import MainHeader from '~/components/MainHeader.vue'
+import ServiceCard from '~/components/ServiceCard.vue'
 
 export default {
   name: 'IndexPage',
   components: {
     MainHeader,
     MyForm,
+    ServiceCard,
   },
 }
 </script>
